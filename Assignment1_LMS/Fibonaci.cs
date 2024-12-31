d5a536c15618fd6bdb56e63b9644fde4979a40e8
swati <swati@SWATI>
2024-12-31 13:34:29 +0530

    Add project files.

---
+++ Assignment1_LMS/Fibonaci.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_LMS
+{
+    internal class Fibonaci
+    {
+
+        static void PrintFibonacciSeries(int N)
+        {
+            int first = 0, second = 1, next;
+
+
+            Console.Write("Fibonacci Series: ");
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (i == 1)
+                {
+                    Console.Write(first + " ");
+                    continue;
+                }
+                if (i == 2)
+                {
+                    Console.Write(second + " ");
+                    continue;
+                }
+
+                next = first + second;
+                first = second;
+                second = next;
+
+                Console.Write(next + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+
+            Console.Write("Enter the number of terms in Fibonacci series: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            PrintFibonacciSeries(N);
+        }
+    }
+}
---
+++ Assignment1_LMS/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment1_LMS
+{
+    internal class Program
+    {
+        static double ConvertToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        static void Main()
+        {
+
+            Console.Write("Enter Cel Temp: ");
+            double celTemp = Convert.ToDouble(Console.ReadLine());
+
+
+            double fahrenheitTemp = ConvertToFahrenheit(celTemp);
+
+
+            Console.WriteLine("Fahrenheit Temperature is : " + fahrenheitTemp);
+        }
+    }
+}
